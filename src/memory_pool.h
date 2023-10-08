1bda408622cc72e0c1911756cfaceeabaca5fc7f
Shaw Hou <houshuosec@163.com>
2023-10-08 23:09:44 +0800

    Initial commit.

---
+++ src/memory_pool.h
@@ -0,0 +1,23 @@
+#ifndef _MEMORY_POOL_H
+#define _MEMORY_POOL_H
+
+#include <stdint.h>
+
+void mp_init(void)
+{
+
+}
+
+void *mp_alloc(uint32_t size)
+{
+
+}
+
+void mp_free(void *ptr)
+{
+
+}
+
+
+
+#endif
